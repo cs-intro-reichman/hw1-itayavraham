f9f8ad561205f8eada3c2627d6c94da38ce68b9e
unknown <itayavschool@gmail.com>
2024-11-04 18:40:41 +0200

    added comment to clarify

--- Bill3.java
+++ Bill3.java
@@ -2,8 +2,8 @@
 public class Bill3 {
 	public static void main(String[] args) {
 	    String name1 = args[0], name2 = args[1], name3 = args[2];
-	    int bill = Integer.parseInt(args[3]);
-            double result = Math.ceil(bill/3.0); //Dividing the integer input by 3 + rounding the result
+	    int bill = Integer.parseInt(args[3]); //Question said to get an integer instead of a double(?)
+        double result = Math.ceil(bill/3.0); //Dividing the integer input by 3 + rounding the result
 	    System.out.println("Dear "+name1+", "+name2+", and "
 			       +name3+": Pay "+result+" shekels each");  
 	}
