df968d1c51f4af380c7b52c7c0efe3c594e37205
unknown <itay.avraham01@post.runi.ac.il>
2024-11-05 13:43:02 +0200

    testing university email

--- Bill3.java
+++ Bill3.java
@@ -4,7 +4,7 @@ public class Bill3 {
 	    String name1 = args[0], name2 = args[1], name3 = args[2];
 	    int bill = Integer.parseInt(args[3]); //Question said to get an integer instead of a double(?)
         double result = Math.ceil(bill/3.0); //Dividing the integer input by 3 + rounding the result
-	    System.out.println("Dear "+name3+", "+name2+", and "
-			       +name1+": pay "+result+" Shekels each");  
+	    System.out.println("Dear "+ name3 +", "+ name2 +", and "
+			       + name1 +": pay "+ result +" shekels each");  
 	}
 }
