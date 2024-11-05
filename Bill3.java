522fbde4d90f4112aa62f715f80d7c9bc9b7c60b
unknown <itayavschool@gmail.com>
2024-11-05 11:31:10 +0200

    minor adjustments to help the autograder

--- Bill3.java
+++ Bill3.java
@@ -4,7 +4,7 @@ public class Bill3 {
 	    String name1 = args[0], name2 = args[1], name3 = args[2];
 	    int bill = Integer.parseInt(args[3]); //Question said to get an integer instead of a double(?)
         double result = Math.ceil(bill/3.0); //Dividing the integer input by 3 + rounding the result
-	    System.out.println("Dear "+name1+", "+name2+", and "
-			       +name3+": Pay "+result+" shekels each");  
+	    System.out.println("Dear "+name3+", "+name2+", and "
+			       +name1+": Pay "+result+" shekels each");  
 	}
 }
